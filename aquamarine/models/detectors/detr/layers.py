dd61b9611476e9d22c5c822f35758e78ec7d43f0
winterchild <jooh@vtouch.io>
2022-04-27 14:15:22 +0900

    Fix `DETRTransformer` class
    * Fix type of `query_pos`

--- aquamarine/models/detectors/detr/layers.py
+++ aquamarine/models/detectors/detr/layers.py
@@ -43,7 +43,7 @@ class DETRTransformer(nn.Module):
             if 1 < p.dim():
                 nn.init.xavier_uniform_(p)
 
-    def forward(self, x, pos: Optional[torch.Tensor], query_pos: torch.Tensor):
+    def forward(self, x, pos: Optional[torch.Tensor], query_pos: Optional[torch.Tensor]):
         encoder_memory = self.encoder(x, pos)
         object_queries = torch.zeros_like(query_pos)
         return self.decoder(object_queries, encoder_memory, pos, query_pos)
