1901524498c158f6ce9ed1155d7e5ecb000f541d
winterchild <jooh@vtouch.io>
2022-04-27 16:04:55 +0900

    Move `Matcher` class to `DETR`

--- aquamarine/modules/losses.py
+++ aquamarine/modules/losses.py
@@ -1,19 +1,5 @@
 import torch
 import torch.nn as nn
-import torchvision.ops.boxes as bx
-
-
-class BoxLoss(nn.Module):
-
-    def __init__(self, lamb_l1: float = 1., lamb_giou: float = 1.):
-        super(BoxLoss, self).__init__()
-        self.lamb_l1 = lamb_l1
-        self.lamb_giou = lamb_giou
-        self.loss_l1 = torch.cdist
-        self.loss_giou = bx.generalized_box_iou
-
-    def forward(self, outputs, targets):
-        pass
 
 
 class MatchLoss(nn.Module):
