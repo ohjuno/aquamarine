23f960bb01fb49c8271ae345ad2b17bfba88ff22
winterchild <jooh@vtouch.io>
2022-04-27 16:47:54 +0900

    Fix `__init__.py` in models

--- aquamarine/models/detectors/__init__.py
+++ aquamarine/models/detectors/__init__.py
@@ -1,4 +1,4 @@
-from aquamarine.models.detectors.detr import DETR, DETRTransformer
+from aquamarine.models.detectors.detr import DETR, DETRTransformer, HungarianMatcher
 
 
-__all__ = ['DETR', 'DETRTransformer']
+__all__ = ['DETR', 'DETRTransformer', 'HungarianMatcher']
