dafe15bc25b1aa72085a99eb23f44bd0f9893061
winterchild <jooh@vtouch.io>
2022-04-28 13:43:41 +0900

    Update detr example code
    * Add every example calls to train `DETR` model
    * Add annotations for code lines

--- example/example_detr.py
+++ example/example_detr.py
@@ -1,15 +1,36 @@
-import torch
 import torch.nn as nn
 import torchvision.models as models
+import torchvision.transforms as transforms
 
+from aquamarine.datasets.coco import COCODetection, COCODataLoader
 from aquamarine.models import DETR, DETRTransformer
+from aquamarine.modules import HungarianLoss, HungarianMatcher
 
 
 if __name__ == '__main__':
+    # configurations
+    root = '/mnt/datasets/coco/train2017'
+    annFile = '/mnt/datasets/coco/annotations/instances_train2017.json'
+    transform = transforms.Compose([transforms.ToTensor()])
+    batch_size = 4
+
+    # dataloader
+    trainset = COCODetection(root=root, annFile=annFile, transform=transform)
+    trainloader = COCODataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=0)
+
+    # model
     backbone = nn.Sequential(*list(models.resnet18().children()))[:-2]
     detector = DETRTransformer(512, 8, 2048, 6, 6)
-    model = DETR(backbone, detector, 20, 100)
+    model = DETR(backbone, detector, num_classes=100, num_queries=100)
 
-    dummies = torch.rand((4, 3, 1600, 1600))
-    outputs = model(dummies)
+    # criterion
+    matcher = HungarianMatcher(lamb_labels=1., lamb_bboxes=5., lamb_geniou=2.)
+    criterion = HungarianLoss(num_classes=100, matcher=matcher, eos_coef=0.1)
+
+    # data
+    inputs, targets = next(iter(trainloader))
+    outputs = model(inputs)
+
+    # debug
+    losses = criterion(outputs, targets)
     breakpoint()
