af7d5fc5621c3b1ff10f606e8940ffe4ccc3b7bf
demie <demie.oh@gmail.com>
2022-06-01 08:07:48 +0900

    Fix `DETR` example code
    * Update dynamic positional encoding size defined by inputs resolution
    * Add `nan_to_num` to handle unexpected nan outputs produced by the model

--- example/example_detr.py
+++ example/example_detr.py
@@ -4,6 +4,7 @@ import torchvision.models as models
 import aquamarine.datasets.coco.transforms as transforms
 
 from aquamarine.datasets.coco import COCODetection, COCODataLoader
+from aquamarine.datasets.utils import visualize_bounding_boxes_on_batch
 from aquamarine.models import DETR, DETRTransformer, HungarianMatcher
 from aquamarine.modules import HungarianLoss
 
@@ -12,13 +13,14 @@ if __name__ == '__main__':
     # configurations
     root = '/mnt/datasets/coco/train2017'
     annFile = '/mnt/datasets/coco/annotations/instances_train2017.json'
-    batch_size = 4
+    batch_size = 16
+    resolution = 640
     num_classes = 100
     num_queries = 100
 
     # dataloader
     coco_transforms = transforms.Compose([
-        transforms.Resize(640),
+        transforms.Resize(resolution),
         transforms.ToTensor(),
         transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
         # `Normalize` converts the format of bounding box from `xyxy` to `cxcywh`
@@ -33,7 +35,7 @@ if __name__ == '__main__':
 
     # object queries & positional encodings
     object_queries = torch.zeros(batch_size, num_queries, 512)
-    pos = nn.Parameter(torch.empty((batch_size, 400, 512)))
+    pos = nn.Parameter(torch.empty((batch_size, int((resolution / 32) ** 2), 512)))
     query_pos = nn.Parameter(torch.empty((batch_size, num_queries, 512)))
 
     # criterion
@@ -43,7 +45,9 @@ if __name__ == '__main__':
     # data
     # target bounding box format: `cxcywh`
     inputs, targets = next(iter(trainloader))
+    visualize_bounding_boxes_on_batch((inputs, targets))
     outputs = model(inputs, object_queries, pos, query_pos)
+    outputs = torch.nan_to_num(outputs, nan=1e-5)
     outputs = torch.split(outputs, [num_classes + 1, 4], dim=-1)
     outputs = dict(labels=outputs[0], bboxes=outputs[1])
 
