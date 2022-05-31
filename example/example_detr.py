3543db52d2c18761728e9d03f70cb555791c6f71
demie <demie.oh@gmail.com>
2022-06-01 07:08:36 +0900

    Update `DETR` example code
    * Add guideline for `transforms`
    * Add object_queries and positional encodings as an independent variables
    * Add post-processing for output to support onnx

--- example/example_detr.py
+++ example/example_detr.py
@@ -1,6 +1,7 @@
+import torch
 import torch.nn as nn
 import torchvision.models as models
-import torchvision.transforms as transforms
+import aquamarine.datasets.coco.transforms as transforms
 
 from aquamarine.datasets.coco import COCODetection, COCODataLoader
 from aquamarine.models import DETR, DETRTransformer, HungarianMatcher
@@ -11,25 +12,40 @@ if __name__ == '__main__':
     # configurations
     root = '/mnt/datasets/coco/train2017'
     annFile = '/mnt/datasets/coco/annotations/instances_train2017.json'
-    transform = transforms.Compose([transforms.ToTensor()])
     batch_size = 4
+    num_classes = 100
+    num_queries = 100
 
     # dataloader
-    trainset = COCODetection(root=root, annFile=annFile, transform=transform)
+    coco_transforms = transforms.Compose([
+        transforms.Resize(640),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
+        # `Normalize` converts the format of bounding box from `xyxy` to `cxcywh`
+    ])
+    trainset = COCODetection(root=root, annFile=annFile, transforms=coco_transforms)
     trainloader = COCODataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=0)
 
     # model
     backbone = nn.Sequential(*list(models.resnet50().children()))[:-2]
     detector = DETRTransformer(512, 8, 2048, 6, 6)
-    model = DETR(backbone, detector, num_classes=100, num_queries=100)
+    model = DETR(backbone, detector, num_classes=num_classes, num_queries=num_queries)
+
+    # object queries & positional encodings
+    object_queries = torch.zeros(batch_size, num_queries, 512)
+    pos = nn.Parameter(torch.empty((batch_size, 400, 512)))
+    query_pos = nn.Parameter(torch.empty((batch_size, num_queries, 512)))
 
     # criterion
     matcher = HungarianMatcher(lamb_labels=1., lamb_bboxes=5., lamb_geniou=2.)
     criterion = HungarianLoss(num_classes=100, matcher=matcher, eos_coef=0.1)
 
     # data
+    # target bounding box format: `cxcywh`
     inputs, targets = next(iter(trainloader))
-    outputs = model(inputs)
+    outputs = model(inputs, object_queries, pos, query_pos)
+    outputs = torch.split(outputs, [num_classes + 1, 4], dim=-1)
+    outputs = dict(labels=outputs[0], bboxes=outputs[1])
 
     # debug
     losses = criterion(outputs, targets)
