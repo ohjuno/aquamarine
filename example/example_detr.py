f2462b4708ed864a1788c0224e2d333d4ff1ab47
demie <demie.oh@gmail.com>
2022-06-01 18:27:49 +0900

    Fix `DETR` example code
    * Offer fully working test code
    * Test device allocation

--- example/example_detr.py
+++ example/example_detr.py
@@ -4,7 +4,6 @@ import torchvision.models as models
 import aquamarine.datasets.coco.transforms as transforms
 
 from aquamarine.datasets.coco import COCODetection, COCODataLoader
-from aquamarine.datasets.utils import visualize_bounding_boxes_on_batch
 from aquamarine.models import DETR, DETRTransformer, HungarianMatcher
 from aquamarine.modules import HungarianLoss
 
@@ -17,6 +16,9 @@ if __name__ == '__main__':
     resolution = 640
     num_classes = 100
     num_queries = 100
+    device = 'cuda:0'
+    dtype = None
+    factory_kwargs = dict(device=device, dtype=dtype)
 
     # dataloader
     coco_transforms = transforms.Compose([
@@ -32,25 +34,34 @@ if __name__ == '__main__':
     backbone = nn.Sequential(*list(models.resnet50().children()))[:-2]
     detector = DETRTransformer(512, 8, 2048, 6, 6)
     model = DETR(backbone, detector, num_classes=num_classes, num_queries=num_queries)
+    model.to(device)
 
     # object queries & positional encodings
-    object_queries = torch.zeros(batch_size, num_queries, 512)
-    pos = nn.Parameter(torch.empty((batch_size, int((resolution / 32) ** 2), 512)))
-    query_pos = nn.Parameter(torch.empty((batch_size, num_queries, 512)))
+    object_queries = torch.zeros(batch_size, num_queries, 512, **factory_kwargs)
+    pos = nn.Parameter(torch.empty((batch_size, int((resolution / 32) ** 2), 512), **factory_kwargs))
+    query_pos = nn.Parameter(torch.empty((batch_size, num_queries, 512), **factory_kwargs))
 
     # criterion
     matcher = HungarianMatcher(lamb_labels=1., lamb_bboxes=5., lamb_geniou=2.)
-    criterion = HungarianLoss(num_classes=100, matcher=matcher, eos_coef=0.1)
+    criterion = HungarianLoss(num_classes=100, matcher=matcher, eos_coef=0.1, **factory_kwargs)
+    # use factory_kwargs to init criterion since it has weights or use criterion.to(device) instead
 
     # data
     # target bounding box format: `cxcywh`
-    inputs, targets = next(iter(trainloader))
-    visualize_bounding_boxes_on_batch((inputs, targets))
-    outputs = model(inputs, object_queries, pos, query_pos)
-    outputs = torch.nan_to_num(outputs, nan=1e-5)
-    outputs = torch.split(outputs, [num_classes + 1, 4], dim=-1)
-    outputs = dict(labels=outputs[0], bboxes=outputs[1])
+    # inputs, targets = next(iter(trainloader))
+    for idx, batch in enumerate(trainloader):
+        inputs, targets = batch
+        inputs = inputs.to(device)
+        targets = [{k: v.to(device) for k, v in target.items() if isinstance(v, torch.Tensor)} for target in targets]
 
-    # debug
-    losses = criterion(outputs, targets)
+        outputs = model(inputs, object_queries, pos, query_pos)
+        outputs = torch.split(outputs, [num_classes + 1, 4], dim=-1)
+        outputs = dict(labels=outputs[0], bboxes=outputs[1])
+
+        # debug
+        losses = criterion(outputs, targets)
+
+        # console printer
+        print(f'\rbatch index #{idx} (max={len(trainloader)}) is checked.', end='')
+
     breakpoint()
